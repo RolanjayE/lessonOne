f86e00f68582212d6b50d4e73d402e70fdfc1885
RolanjayE <rolanjayisabida46@gmail.com>
2024-03-19 05:35:38 +0700

    second commit

---
+++ javascript.js
@@ -0,0 +1,6 @@
+// How to structure our file.
+// How to create our repository.
+// Push our file to GitHub using Git.
+// Deploy our file (meaning it will be accessible on the internet).
+// Add a new feature to our file.
+// How to save again and push again to our GitHub.
